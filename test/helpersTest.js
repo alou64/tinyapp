a9bcdb8aafb87c2a7005a8c870ba0654721f7936
alou <01008@protonmail.com>
2022-01-27 12:45:25 -0800

    implemented test for urlsForUser function

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,5 @@
 const { assert } = require('chai');
-const { getUserByEmail } = require('../constants/helperFunctions');
-//const { users, urlDatabase } = require('../constants/data');
+const { getUserByEmail, urlsForUser } = require('../constants/helperFunctions');
 
 
 const testUsers = {
@@ -13,6 +12,18 @@ const testUsers = {
     id: "user2RandomID",
     email: "user2@example.com",
     password: "dishwasher-funk"
+  }
+};
+
+
+const testURLs = {
+  'b2xVn2': {
+    longURL: 'http://www.lighthouselabs.ca',
+    userID: 'userRandomID'
+  },
+  '9sm5xK': {
+    longURL: 'http://www.google.com',
+    userID: 'userRandomID'
   }
 };
 
@@ -30,5 +41,23 @@ describe('getUserByEmail', () => {
   it('should return false when passed an email not in the database', () => {
     const user = getUserByEmail("user5@example.com", testUsers)
     assert.isFalse(user);
+  });
+});
+
+
+describe('urlsForUser', () => {
+  it('should return an object of urls owned by the user', () => {
+    const urls = urlsForUser('userRandomID', testURLs);
+    const expected = { 'b2xVn2': 'http://www.lighthouselabs.ca', '9sm5xK': 'http://www.google.com' };
+    assert.deepEqual(urls, expected);
+  });
+});
+
+
+describe('urlsForUser', () => {
+  it('should return an empty object when passed an id that does not own any urls', () => {
+    const urls = urlsForUser('userRandomID5', testURLs);
+    const expected = {};
+    assert.deepEqual(urls, expected);
   });
 });
