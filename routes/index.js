b44182a2d4460c23b01a6039e762a3e379cf779e
alou <01008@protonmail.com>
2022-02-01 07:07:58 -0800

    removed alert

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
   // check if user logged in
   // redirect to login page if not logged in
   if (!req.session.user_id) {
-    return res.redirect('/login/?alert=notLoggedIn');
+    return res.redirect('/login');
   }
 
   // redirect to /urls
